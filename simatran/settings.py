876a8e2f90873fbde4ccca47959f2156909a4e48
bigolho16 <bigolho900@gmail.com>
2023-05-18 21:22:23 -0300

    Envio Inicial (Com Msg de Manutenção) v0.0.7

--- simatran/settings.py
+++ simatran/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sistema.apps.SistemaConfig',
-    'usuarios.apps.UsuariosConfig'
+    # 'usuarios.apps.UsuariosConfig'
 ]
 
 MIDDLEWARE = [
